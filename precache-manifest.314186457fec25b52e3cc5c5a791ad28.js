9f79df450fb381e8fa661f7cd321c2f23b31fb6d
SAIF0206 <msaif.sam@gmail.com>
2020-03-23 17:49:45 +0800

    Updates

---
+++ precache-manifest.314186457fec25b52e3cc5c5a791ad28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07bbfc7b423aa65d17af10bd1ce69394",
+    "url": "/CRUD-ReactJS/index.html"
+  },
+  {
+    "revision": "b8b3da9f25e0ad1e88a0",
+    "url": "/CRUD-ReactJS/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e40465af1d35a59ac40e",
+    "url": "/CRUD-ReactJS/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b8b3da9f25e0ad1e88a0",
+    "url": "/CRUD-ReactJS/static/js/2.a379e339.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/CRUD-ReactJS/static/js/2.a379e339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40465af1d35a59ac40e",
+    "url": "/CRUD-ReactJS/static/js/main.39cd2343.chunk.js"
+  },
+  {
+    "revision": "e5f3cfec454b5f442274",
+    "url": "/CRUD-ReactJS/static/js/runtime-main.97dc35df.js"
+  }
+]);
